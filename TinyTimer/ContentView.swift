39f05743e67e1193c51d5397f2a8c522498ebe93
Tomer Sedan <tomer.sedan@protonmail.com>
2021-04-09 13:36:02 -0700

    Initial Commit

---
+++ TinyTimer/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  TinyTimer
+//
+//  Created by Tomer Sedan on 4/9/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
